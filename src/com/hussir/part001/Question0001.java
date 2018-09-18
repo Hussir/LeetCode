cf54165e08dd672ea7cf62714435db112cd68715
HeiLi <18991492@qq.com>
2018-09-18 14:38:55 +0800

    Solve the 1th algorithm problem

---
+++ src/com/hussir/part001/Question0001.java
@@ -0,0 +1,24 @@
+package com.hussir.part001;
+
+import java.util.HashMap;
+
+public class Question0001 {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0) return null;
+
+        HashMap<Integer, Integer> assistMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (assistMap.containsKey(target - nums[i])) {
+                return new int[]{assistMap.get(target - nums[i]), i};
+            } else {
+                assistMap.put(nums[i], i);
+            }
+        }
+
+        return null;
+    }
+}
